7a6a2dd98d7463e9b9d84fd6b54cec0f2d729b74
shinesoleil <hao_pan@outlook.com>
2016-03-09 15:56:17 +0800

    fix JSON parse bug

--- js/itemList.js
+++ js/itemList.js
@@ -38,50 +38,12 @@ function initialiseItems() {
     }
   ];
 
-  setData("items", items);
+  setData("items", JSON.stringify(items));
 }
+var test;
 
-
-function appendItems() {
-
-  var items = [
-    {
-      "id": "0",
-      "name": "可口可乐",
-      "unit": "瓶",
-      "price": 3.00
-    },
-    {
-      "id": "1",
-      "name": "雪碧",
-      "unit": "瓶",
-      "price": 3.00
-    },
-    {
-      "id": "2",
-      "name": "苹果",
-      "unit": "斤",
-      "price": 5.50
-    },
-    {
-      "id": "3",
-      "name": "荔枝",
-      "unit": "斤",
-      "price": 15.00
-    },
-    {
-      "id": "4",
-      "name": "电池",
-      "unit": "个",
-      "price": 2.00
-    },
-    {
-      "id": "5",
-      "name": "方便面",
-      "unit": "袋",
-      "price": 4.50
-    }
-  ];
+function appendItems(items) {
+  items = JSON.parse(items);
 
   items.forEach(function (item) {
     $('table').append(
@@ -98,4 +60,7 @@ function appendItems() {
   })
 }
 
-appendItems();
+if (getData("items") == null) {
+  initialiseItems();
+}
+appendItems(getData("items"));
