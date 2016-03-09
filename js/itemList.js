08f361135d753ab6cf51f3e1333cc67feadafac1
shinesoleil <hao_pan@outlook.com>
2016-03-09 15:09:30 +0800

    add plus and minus glyphs in buttons

--- js/itemList.js
+++ js/itemList.js
@@ -40,7 +40,6 @@ function appendItems() {
   ];
 
   items.forEach(function (item) {
-
     $('table').append(
       '<tr>' +
       '<td>' + item.name + '</td>' +
@@ -48,8 +47,8 @@ function appendItems() {
       '<td>' + item.unit + '</td>' +
       '<td>0</td>' +
       '<td>' +
-      '<button>-</button>' +
-      '<button>+</button>' +
+      '<button class="btn btn-warning btn-xs"><span class="glyphicon glyphicon-minus"></span></button> ' +
+      '<button class="btn btn-success btn-xs"><span class="glyphicon glyphicon-plus"></span></button>' +
       '</td>' +
       '</tr>')
   })
