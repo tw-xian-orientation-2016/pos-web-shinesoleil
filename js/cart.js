fb6559e5bb3b9a1e96455731ba13fe2890c0cdbf
shinesoleil <hao_pan@outlook.com>
2016-03-09 15:31:48 +0800

    cart page done

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,60 @@
-/**
- * Created by hao on 3/9/16.
- */
+function appendCart() {
+
+  var items = [
+    {
+      "id": "0",
+      "name": "可口可乐",
+      "unit": "瓶",
+      "price": 3.00
+    },
+    {
+      "id": "1",
+      "name": "雪碧",
+      "unit": "瓶",
+      "price": 3.00
+    },
+    {
+      "id": "2",
+      "name": "苹果",
+      "unit": "斤",
+      "price": 5.50
+    },
+    {
+      "id": "3",
+      "name": "荔枝",
+      "unit": "斤",
+      "price": 15.00
+    },
+    {
+      "id": "4",
+      "name": "电池",
+      "unit": "个",
+      "price": 2.00
+    },
+    {
+      "id": "5",
+      "name": "方便面",
+      "unit": "袋",
+      "price": 4.50
+    }
+  ];
+
+  items.forEach(function (item) {
+    $('table').append(
+      '<tr>' +
+      '<td>' + item.name + '</td>' +
+      '<td>' + item.price + '</td>' +
+      '<td>' + item.unit + '</td>' +
+      '<td>0</td>' +
+      '<td>0</td>' +
+      '<td>' +
+      '<button class="btn btn-warning btn-xs"><span class="glyphicon glyphicon-remove"></span></button> ' +
+      '<button class="btn btn-success btn-xs"><span class="glyphicon glyphicon-minus"></span></button> ' +
+      '<button class="btn btn-success btn-xs"><span class="glyphicon glyphicon-plus"></span></button>' +
+      '</td>' +
+      '</tr>')
+  })
+
+}
+
+appendCart();
