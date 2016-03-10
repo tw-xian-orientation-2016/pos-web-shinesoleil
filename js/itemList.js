28e04bebeac42f159bdee9e0dc81eb7f3556272e
shinesoleil <hao_pan@outlook.com>
2016-03-10 11:29:55 +0800

    move initialise functions to initialise.js

--- js/itemList.js
+++ js/itemList.js
@@ -1,68 +1,3 @@
-function initialiseItems() {
-  var items = [
-    {
-      "id": "0",
-      "name": "可口可乐",
-      "unit": "瓶",
-      "price": 3.00
-    },
-    {
-      "id": "1",
-      "name": "雪碧",
-      "unit": "瓶",
-      "price": 3.00
-    },
-    {
-      "id": "2",
-      "name": "苹果",
-      "unit": "斤",
-      "price": 5.50
-    },
-    {
-      "id": "3",
-      "name": "荔枝",
-      "unit": "斤",
-      "price": 15.00
-    },
-    {
-      "id": "4",
-      "name": "电池",
-      "unit": "个",
-      "price": 2.00
-    },
-    {
-      "id": "5",
-      "name": "方便面",
-      "unit": "袋",
-      "price": 4.50
-    }
-  ];
-
-  setData("items", JSON.stringify(items));
-}
-
-function initialiseCart(items) {
-
-  var cartItems = [];
-
-  items.forEach(function (item) {
-    cartItems.push({
-      "id": item.id,
-      "count": 0,
-      "subtotal": 0
-    });
-  });
-
-  setData("cart", JSON.stringify(cartItems));
-}
-
-function initialse() {
-  initialiseItems();
-  initialiseCart(JSON.parse(getData("items")));
-}
-
-
-
 function appendItems(items, cart) {
 
   items.forEach(function (item) {
@@ -81,12 +16,18 @@ function appendItems(items, cart) {
 }
 
 
+$( document ).ready(function() {
+  if (getData("items") === null) {
+    initialse();
+  }
 
-if (getData("items") === null) {
-  initialse();
-}
+  if(getData("cart") === null) {
+    initialse();
+  }
 
-if(getData("cart") === null) {
-  initialse();
-}
-appendItems(JSON.parse(getData("items")), JSON.parse(getData("cart")));
+  appendItems(JSON.parse(getData("items")), JSON.parse(getData("cart")));
+
+  addButton();
+  minusButton();
+});
+
