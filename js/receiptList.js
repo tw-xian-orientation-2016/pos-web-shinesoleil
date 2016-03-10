a54a200e89c5cfbf3e7cb21dcaac003f4b2f0378
shinesoleil <hao_pan@outlook.com>
2016-03-10 11:14:02 +0800

    add detail button

--- js/receiptList.js
+++ js/receiptList.js
@@ -1,3 +1,12 @@
-/**
- * Created by hao on 3/10/16.
- */
+function appendReceipt(receipts) {
+  receipts.forEach(function (receipt) {
+    $('table').append(
+      '<tr>' +
+      '<td>' + receipt.time + '</td>' +
+      '<td>' + receipt.total + '</td>' +
+      '<td><button class="btn btn-success btn-xs minus" id=' + receipt.time + '><span class="glyphicon glyphicon-zoom-in"></span> Detail</button></td>' +
+      '</tr>')
+  })
+}
+
+appendReceipt(JSON.parse(getData("receipts")));
